899198244746de68ce22873d1dc314cfef8656e3
Infinity <anantshreey@gmail.com>
2025-09-29 23:56:42 +0530

    compares 3 integers and tells which one is greater

---
+++ C and C++/compare3.c
@@ -0,0 +1,24 @@
+//Write a program that finds the largest of three numbers.
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    int c;
+    printf("Provided three integer to compare which one is the greatest: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b && a > c)
+    {
+        printf("%d is the greatest among the three integers\n", a);
+    }
+    else if(b > a && b > c)
+    {
+        printf("%d is the greatest among the three integers\n", b);
+    }
+    else if(c > a && c > b)
+    {
+        printf("%d is the greatest among the three integers\n", c);
+    }
+}
