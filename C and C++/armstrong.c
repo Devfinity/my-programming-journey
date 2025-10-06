705bcb44a71002d3c8f9dad1d3c4e64a1cbd9f9b
Infinity <anantshreey@gmail.com>
2025-10-06 23:14:30 +0530

    tells if the given integer is armstrong number or not

---
+++ C and C++/armstrong.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) 
+{ 
+    long original; printf("Provide a integer: ");
+    scanf("%ld", &original); 
+    
+    long last_digit;
+    long copy = original;
+    long copy1 = original;
+    long power = 0;
+
+    while(original != 0)
+    { 
+        last_digit = original % 10;
+        original = original / 10;
+        power++;
+    } 
+    long sum = 0;
+    
+    while(copy != 0)
+    {
+        last_digit = copy % 10;
+        sum = sum + pow(last_digit, power);
+        copy = copy / 10;
+    }
+    if(sum == copy1)
+    {
+        printf("%ld is a armstrong number\n", copy1);
+    }
+    else
+    {
+        printf("%ld is not a armstrong number\n", copy1);
+    }
+}
