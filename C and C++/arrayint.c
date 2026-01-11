8f286e4a3ced123b3e8b8b4c6b1093f61d7c073e
Infinity <anantshreey@gmail.com>
2026-01-11 23:56:38 +0530

    added the question

--- C and C++/arrayint.c
+++ C and C++/arrayint.c
@@ -6,28 +6,29 @@ int main(void)
     long x;
     
     printf("Length of array: ");
-    scanf("%ld", &x);
+    scanf("%ld", &x); //we got the length of array here
 
     long array[x];
     long i;
 
     for(i = 0;i < x; i++ )
     {
-        scanf("%ld", &array[i]);
+        scanf("%ld", &array[i]); //user giving values of array
     }
     printf("\n");
     long y;
     long pass;
+    //bubble sort needs 2 for loops to work
 
-    for(pass = 0; pass < x - 1; pass++)
+    for(pass = 0; pass < x - 1; pass++) //puts array elements in its correct position
     {
-        for(i = 0; i < x - pass - 1; i++)
+        for(i = 0; i < x - pass - 1; i++) //looping array to compare
         {
-            if(array[i] > array[i + 1])
+            if(array[i] > array[i + 1]) //actual comparition
             {
-            y = array[i];
-            array[i] = array [i + 1];
-            array[i + 1] = y;
+            y = array[i]; //holds one value
+            array[i] = array [i + 1]; //move the next one to left
+            array[i + 1] = y; //put them back at right
             }
         }
     }
