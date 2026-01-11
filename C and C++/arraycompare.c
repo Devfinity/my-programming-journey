fb2d160bb6ba7ee72f6faae033251f36ae8cd31d
Infinity <anantshreey@gmail.com>
2026-01-11 21:00:27 +0530

    In this code we use array to find when bob and alice are both happy using a equation

---
+++ C and C++/arraycompare.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+     
+    printf("Number of testcases: ");
+    scanf("%d", &t);
+
+    while (t--) {
+        int n,j,i;
+
+        printf("Number of days they ran: ");
+        scanf("%d", &n);
+        int a[n], b[n];
+        
+        printf("Alice ran for(in meters): ");
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        
+        printf("Bob ran for(in meters): ");
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &b[i]);
+        }
+        int happymax = 0;
+        int bob = 0;
+        int alice = 0;
+        
+        for(j = 0; j < n; j++)
+        {
+            if (a[j] * 2 < b[j])
+            {
+                bob++;
+            }
+            else if(a[j] > 2 * b[j])
+            {
+                alice++;
+            }
+            else
+            {
+                happymax++;
+            }
+        }
+        printf("%d\n", happymax);
+    }
+    
+    return 0;
+}
