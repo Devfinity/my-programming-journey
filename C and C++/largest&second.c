562164514b7217a6210f527aff55e696c2ed93fc
Infinity <anantshreey@gmail.com>
2026-01-12 15:29:43 +0530

    finds the largest and second largest in a array and does addition of the values

---
+++ C and C++/largest&second.c
@@ -0,0 +1,41 @@
+//find the largest and second largest in a array and print out the sum
+
+#include <stdio.h>
+
+int main() {
+    int t,j;
+    
+    printf("Number of testcases: ");
+    scanf("%d", &t);
+
+    while (t--) {
+        int n,i;
+        
+        printf("Number of Elemnts: ");
+        scanf("%d", &n);
+        int a[n];
+
+        printf("Elements: ");
+        for (i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        int temp = a[0];
+        int second = a[0];
+
+        for(i = 1; i < n; i++)
+        {
+            if(a[i] > temp)
+            {
+                temp = a[i];
+            }    
+            
+            else if (temp > a[i] && a[i] > second)
+            {
+                second = a[i];
+            }
+        }
+        printf("%d\n", temp + second);
+    }
+
+    return 0;
+}
