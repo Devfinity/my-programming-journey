0cff97f6327bc18bfebf4cca6b05b642b707b7f5
Infinity <anantshreey@gmail.com>
2026-01-25 01:57:14 +0530

    added some comments so i don't forget the concept

--- C and C++/titlecase.c
+++ C and C++/titlecase.c
@@ -5,24 +5,24 @@
 int main() {
     int t;
     	
-    scanf("%d\n", &t);
-    	
+    scanf("%d\n", &t); //the "\n" is need so we don't get issue for putting scanf and fgets 
+    	               //next to each other
     while(t > 0)
     {
         t--;
         char s[1001];
-        int i, isacronym = 0;
+        int i, isacronym = 0; //flag once again
         
-        fgets(s, 1001, stdin);
+        fgets(s, 1001, stdin); //this is a sentence!
         
-        char *token = strtok(s, " \n");
+        char *token = strtok(s, " \n"); // token is one word!
         
         while (token != NULL)
         {
             for(i = 0; token[i] != '\0'; i++)
             {
                 isacronym = 0;
-                if(islower(token[i]))
+                if(islower(token[i])) //token[i] is one letter!
                 {
                     isacronym = 1;
                     break;
